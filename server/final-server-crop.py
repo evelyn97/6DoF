df3ce9c0cda29de480199f7b7bbcd1818ed6fbf1
Ying Wang <ying@Ying.local>
2018-08-03 12:20:38 -0700

    test

--- server/final-server-crop.py
+++ server/final-server-crop.py
@@ -1,16 +1,16 @@
 import os
 import sys
 import shutil
-import subprocess
+import json
 from subprocess import check_output
 from pdb import set_trace as st
 import numpy as np
 # from Naked.toolshed.shell import execute_rb, muterun_rb
 
 urlmap = {}
-start_id = 20000
-end_id = 50000
-PATH = '/projects/grail/6DOFnb/NYPL/'
+start_id = 1
+end_id = 100
+PATH = '/projects/grail/6DOFnb/NYPL_new/'
     #sys.argv[1]  # /Users/ying/documents/uw/Junior-quarter4/lab/
                     # /projects/grail/6DOF/dataset_NYPL
 # COMMAND = sys.argv[2]  # '/Users/ying/documents/uw/Junior-quarter4/lab/torch-warp-master/nypl_recrop.rb'
@@ -58,7 +58,7 @@ def main():
         # match the images that have common high-res url
         # the shell command
 
-        command = '/projects/grail/6DOF/dataset_NYPL/torch-warp-master/final_nypl_recrop.rb'
+        command = '/projects/grail/6DOF/dataset_NYPL/6DoF/server/final_nypl_recrop.rb'
         # from subprocess import check_output
         # url = check_output([command, str(id)])
 
@@ -89,18 +89,30 @@ def main():
         #     shutil.rmtree(idDir)
         if len(os.listdir(cropDir + str(id))) == 0:
             shutil.rmtree(cropDir + str(id))
-        else:
-            remove1 = PATH + str(id) + '.jpg'
+        
+        remove1 = PATH + str(id) + '.jpg'
+        if os.path.exists(remove1):
             os.remove(remove1)
-            remove2 = PATH + str(id) + '_cropped.tif'
+        remove2 = PATH + str(id) + '_cropped.tif'
+        if os.path.exists(remove2):
             os.remove(remove2)
-            remove3 = PATH + str(id) + '_gif.jpg'
+        remove3 = PATH + str(id) + '_gif.jpg'
+        if os.path.exists(remove3):
             os.remove(remove3)
 
         if id % 1000 == 0:
             # log url map
-            sys.stdout = open(urlDir + 'urlmap_' + str(id) + '.txt', 'wt')
-            print urlmap
+            jsonmap = json.dumps(urlmap)
+            filename = urlDir + 'urlmap_' + str(id) + '.json'
+            try:
+                f = open(filename, "w+")
+                f.write(jsonmap)
+                f.close()
+            except OSError as err:
+                print("OS error: {0}".format(err))
+            except:
+                print("Unexpected error:", sys.exc_info()[0])
+                raise
 
 if __name__ == '__main__':
     main()
--- server/final_nypl_recrop.rb
+++ server/final_nypl_recrop.rb
@@ -19,12 +19,12 @@ unless stereo_metadata['external_id'] == 0
 end
 
 gif_url = stereo_metadata['aws_url']
-`wget -nc -O /projects/grail/6DOFnb/NYPL/#{ARGV[0]}_gif.jpg '#{gif_url}'`
+`wget -nc -O #{ARGV[0]}_gif.jpg '#{gif_url}'`
 
-gifName = '/projects/grail/6DOFnb/NYPL/' + ARGV[0] + '_gif.jpg'
+gifName = ARGV[0] + '_gif.jpg'
 size = File.size(gifName)
-if sizei.nil?
-  puts image_captures.to_json
+if size==0 
+  # puts image_captures.to_json
   abort("Low-quality cropping for #{ARGV[0]}")
 end
 
@@ -48,19 +48,19 @@ if matching_captures && matching_captures.length > 0
 
   # download images
   # $stdout.puts "Downloading images..."
-  `wget -nc -O /projects/grail/6DOFnb/NYPL/#{ARGV[0]}.jpg '#{lowres_url}'`
-  `wget -nc -O /projects/grail/6DOFnb/NYPL/tif/#{ARGV[0]}.tif '#{highres_url}'`
+  `wget -nc -O /projects/grail/6DOFnb/NYPL_new/#{ARGV[0]}.jpg '#{lowres_url}'`
+  `wget -nc -O /projects/grail/6DOFnb/NYPL_new/tif/#{ARGV[0]}.tif '#{highres_url}'`
 
   # calculate the crop for the original image using multiscale template matching
   # $stdout.puts "Calculating crop..."
-  crop_params = `./template_match_multiscale.py --template /projects/grail/6DOFnb/NYPL/#{ARGV[0]}.jpg --image /projects/grail/6DOFnb/NYPL/tif/#{ARGV[0]}.tif`.chomp
+  crop_params = `./template_match_multiscale.py --template /projects/grail/6DOFnb/NYPL_new/#{ARGV[0]}.jpg --image /projects/grail/6DOFnb/NYPL_new/tif/#{ARGV[0]}.tif`.chomp
 
   # apply the crop
-  `convert /projects/grail/6DOFnb/NYPL/tif/#{ARGV[0]}.tif -crop #{crop_params} +repage /projects/grail/6DOFnb/NYPL/#{ARGV[0]}_cropped.tif`
+  `convert /projects/grail/6DOFnb/NYPL_new/tif/#{ARGV[0]}.tif -crop #{crop_params} +repage /projects/grail/6DOFnb/NYPL_new/#{ARGV[0]}_cropped.tif`
 
   # calculate dimensions
-  lowres_dims = Dimensions.dimensions("/projects/grail/6DOFnb/NYPL/#{ARGV[0]}.jpg")
-  highres_dims = Dimensions.dimensions("/projects/grail/6DOFnb/NYPL/#{ARGV[0]}_cropped.tif")
+  lowres_dims = Dimensions.dimensions("/projects/grail/6DOFnb/NYPL_new/#{ARGV[0]}.jpg")
+  highres_dims = Dimensions.dimensions("/projects/grail/6DOFnb/NYPL_new/#{ARGV[0]}_cropped.tif")
 
   # calculate scaling
   x_scale = highres_dims[0].to_f / lowres_dims[0].to_f
@@ -75,7 +75,10 @@ if matching_captures && matching_captures.length > 0
   y2 = stereo_metadata['y2'] * y_scale
 
   # store the metadata
-  File.open("/projects/grail/6DOFnb/NYPL/meta/meta_#{ARGV[0]}.txt", 'w+') {|f| f.write(stereo_metadata) }
+  File.open("/projects/grail/6DOFnb/NYPL_new/meta/meta_#{ARGV[0]}.json","w") do |f|
+    f.write(stereo_metadata.to_json)
+  end
+  # File.open("/projects/grail/6DOFnb/NYPL/meta/meta_#{ARGV[0]}.txt", 'w+') {|f| f.write(stereo_metadata) }
 
   # store the url
   # File.open("/Users/ying/documents/uw/Junior-quarter4/lab/url/hiurl_#{ARGV[0]}.txt", 'w+') {|f| f.write(highres_url) }
@@ -90,9 +93,9 @@ if matching_captures && matching_captures.length > 0
   
   # Dir.chdir "/Users/ying/documents/uw/Junior-quarter4/lab/#{ARGV[0]}"# # change directory
 
-  `convert /projects/grail/6DOFnb/NYPL/#{ARGV[0]}_cropped.tif -crop #{cropped_width}x#{cropped_height}+#{x1}+#{y1} +repage /projects/grail/6DOFnb/NYPL/cropped/#{ARGV[0]}/imL.png`
+  `convert /projects/grail/6DOFnb/NYPL_new/#{ARGV[0]}_cropped.tif -crop #{cropped_width}x#{cropped_height}+#{x1}+#{y1} +repage /projects/grail/6DOFnb/NYPL_new/cropped/#{ARGV[0]}/imL.png`
 
-  `convert /projects/grail/6DOFnb/NYPL/#{ARGV[0]}_cropped.tif -crop #{cropped_width}x#{cropped_height}+#{x2}+#{y2} +repage /projects/grail/6DOFnb/NYPL/cropped/#{ARGV[0]}/imR.png`
+  `convert /projects/grail/6DOFnb/NYPL_new/#{ARGV[0]}_cropped.tif -crop #{cropped_width}x#{cropped_height}+#{x2}+#{y2} +repage /projects/grail/6DOFnb/NYPL_new/cropped/#{ARGV[0]}/imR.png`
 
   $stdout.puts highres_url
 else
